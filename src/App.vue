2d5d094fa867d645315dfc20bc881f39c12c2813
nickolay <kolanlos510@gmail.com>
2019-10-11 18:44:30 +0300

    add base code with codepen

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,26 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Circle></Circle>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Circle from './components/circle.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Circle
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+  
+  $color-bg: #212F3D;
+  
+  body {
+    padding: 50px;
+    background:$color-bg;
+  }
 </style>
