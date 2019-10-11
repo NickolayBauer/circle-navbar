4954961ee4801e2ffbd7a536675d13ca50a6f793
nickolay <kolanlos510@gmail.com>
2019-10-11 19:21:51 +0300

    :v0.1

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,40 @@
 <template>
-  <div id="app">
-    <Circle></Circle>
-  </div>
+    <div id="app">
+
+        <Circle_bar
+               from_="64" 
+               to_="256 "
+        />
+        
+        <Circle_bar
+               from_="128" 
+               to_="256"
+        />
+
+      <Circle_bar
+               from_="256" 
+               to_="256"
+        />
+
+    </div>
 </template>
 
 <script>
-import Circle from './components/circle.vue'
+import Circle_bar from "@/components/circle_bar";
 
 export default {
-  name: 'app',
-  components: {
-    Circle
-  }
-}
+    name: "app",
+    components: {
+        Circle_bar
+    }
+};
 </script>
 
 <style lang="scss">
-  
-  $color-bg: #212F3D;
-  
-  body {
+$color-bg: #212f3d;
+
+body {
     padding: 50px;
-    background:$color-bg;
-  }
+    background: $color-bg;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
+import {TweenLite, TweenMax} from 'gsap'
 
 Vue.config.productionTip = false
 
